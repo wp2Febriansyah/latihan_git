3df993f4c719b5a0bf5544c86de58861a6c02b9e
wp2Febriansyah <91050444+wp2Febriansyah@users.noreply.github.com>
2021-09-20 15:48:15 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Febriansyah
+            Saya tinggal di daerah Bantar Kambing
+            Olahraga yang saya sukai adalah Basket";
+    }
+}
